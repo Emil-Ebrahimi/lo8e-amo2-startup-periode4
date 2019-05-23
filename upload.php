e94cd5e07b07bdbf58daf78bc9a63a185b98d4d8
Emil-Ebrahimi <emil.ebrahimi@hotmail.com>
2019-05-23 19:15:34 +0200

    bugfix
    bugfix nadat braas heel mn website heeft gesloopt

--- upload.php
+++ upload.php
@@ -40,7 +40,7 @@ if ($uploadOk == 0) {
       $sql = "INSERT INTO images (image) VALUES ('$filename')";
       $conn->query($sql);
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        header("Location: ../lo8e-amo2-startup-periode4/gallery.php");
+        header("Location: gallery.php");
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
